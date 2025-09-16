6955e9ec5d7da14c1dac39292010eb49b67f534e
Amid Ahadi (Eito) <amid.ahadi@gmail.com>
2025-09-16 12:36:47 +0330

    Add files via upload

---
+++ admin/sidebar.php
@@ -0,0 +1,19 @@
+<div class="position-sticky" style="top: 2rem;">
+    <div class="list-group" id="admin-menu">
+        <a href="#" class="list-group-item list-group-item-action d-flex align-items-center" data-target="dashboard">
+            📊 داشبورد
+        </a>
+        <a href="#" class="list-group-item list-group-item-action d-flex align-items-center" data-target="manage-users">
+            👥 مدیریت کاربران
+        </a>
+        <a href="#" class="list-group-item list-group-item-action d-flex align-items-center" data-target="manage-specialties">
+            🩺 مدیریت تخصص‌ها
+        </a>
+        <a href="#" class="list-group-item list-group-item-action d-flex align-items-center" data-target="manage-doctors">
+            👨‍⚕️ مدیریت پزشکان
+        </a>
+        <a href="#" class="list-group-item list-group-item-action d-flex align-items-center" data-target="manage-availability">
+            ⏰ مدیریت ساعت کاری
+        </a>
+    </div>
+</div>
